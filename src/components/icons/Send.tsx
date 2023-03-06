fdfa47a1006866e76521aa0300bba4a746dd895d
echostars <40875005+echostars@users.noreply.github.com>
2023-03-06 20:46:25 +0800

    Update Send.tsx

--- src/components/icons/Send.tsx
+++ src/components/icons/Send.tsx
@@ -1,5 +1,5 @@
 export default () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 32 32"><path fill="currentColor" d="M9 11.5V19H5.2L9 11.5M11 3L2 21h9V3zM22 21h-9V3z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"/></svg>
   )
 }
