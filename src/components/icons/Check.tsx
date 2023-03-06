93cd3cee0e436bef5ba3317a56164a9405b9253c
echostars <40875005+echostars@users.noreply.github.com>
2023-03-06 20:52:48 +0800

    Update and rename check.tsx to Check.tsx

---
+++ src/components/icons/Check.tsx
@@ -0,0 +1,5 @@
+export default () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path></svg>
+  )
+}
